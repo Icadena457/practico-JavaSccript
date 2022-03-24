3c7f7016bc2036943b887b4e4f9c981e3b67c673
Icadena <icadena457@gmail.com>
2022-03-24 12:56:11 -0500

    Segundo proyecto, sin css

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 - Segundo paso: Implementar fórmulas en JavaScript
 - Tercer paso: Crear funciones
 - Cuarto paso: Integrar JavaScript con HTML 
+
+## Taller #2: Porcentajes y descuentos
+
+- Priemr paso: Definir las fórmulas.
+- Segundo paso: Implementras las fórmulas en JavaScript
+- Tercer paso: Crear funciones.
+- Cuarto paso: Integrar JS con HTML
---
+++ descuentos.js
@@ -0,0 +1,20 @@
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;    
+
+    return precioConDescuento;
+}
+
+function priceDiscount() {
+    const inputPrice = document.getElementById("input-price");
+    const pricevalue = Number(inputPrice.value);
+
+    const inputDiscount = document.getElementById("input-discount");
+    const discountvalue = Number(inputDiscount.value);
+
+    const precioConDescuento = calcularPrecioConDescuento(pricevalue, discountvalue);
+
+    const resultP= document.getElementById("ResultPrice");
+    resultP.innerText = "El precio total de tu producto es: " + precioConDescuento;
+}
