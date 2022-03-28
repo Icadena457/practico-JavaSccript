dfd40f672107c5a2346632571dd52d85b45344f8
Icadena <icadena457@gmail.com>
2022-03-28 09:37:09 -0500

    Segundo proyecto terminado, sin css

--- descuentos.js
+++ descuentos.js
@@ -6,15 +6,36 @@ function calcularPrecioConDescuento(precio, descuento) {
     return precioConDescuento;
 }
 
+const cupones = [
+    "Para_estudiantes",
+    "Para_empresas",
+    "Para_programadores",
+];
+
 function priceDiscount() {
     const inputPrice = document.getElementById("input-price");
     const pricevalue = Number(inputPrice.value);
 
-    const inputDiscount = document.getElementById("input-discount");
-    const discountvalue = Number(inputDiscount.value);
+    const inputCupon = document.getElementById("input-cupons");
+    const cuponvalue = Number(inputCupon.value);
+    
+    let descuento;
 
-    const precioConDescuento = calcularPrecioConDescuento(pricevalue, discountvalue);
+    switch(cuponvalue) {
+        case cupones[0]:
+            descuento = 45;
+            break;
+        case cupones[1]:
+            descuento = 30;
+            break;
+        case cupones[2]:
+            descuento = 35;
+            break;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(pricevalue, cuponvalue);
 
     const resultP= document.getElementById("ResultPrice");
     resultP.innerText = "El precio total de tu producto es: $" + precioConDescuento;
+
 }
